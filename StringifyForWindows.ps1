b6ccac21e5c0e019eba9ec9cb3e9ea9044111851
Oskar Edbro <oskar@edbro.se>
2019-07-07 18:56:02 +0200

    Fixed bug not working with images

--- StringifyForWindows.ps1
+++ StringifyForWindows.ps1
@@ -11,10 +11,12 @@
         }
 
         # Convert to bytes
-        $bytes = [System.Text.Encoding]::Ascii.GetBytes($args[0])
+        # $bytes = $args[0] # [System.Text.Encoding]::Ascii.GetBytes($args[0])
 
         # base64Encode the content
-        $EncodedText =[Convert]::ToBase64String($bytes)
+        # $EncodedText =[Convert]::ToBase64String($bytes)
+
+        $EncodedText = [Convert]::ToBase64String($args[0])
 
         return $EncodedText
     }
@@ -24,7 +26,7 @@
             throw 'invalid variable passed to base64Deccode'
         }
 
-        $DecodedText = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String($args[0]))
+        $DecodedText = [System.Convert]::FromBase64String($args[0])
         return $DecodedText
     }
 
@@ -37,7 +39,8 @@
         $filePath = $args[0]
         $fileName = [regex]::match($filePath, '([a-zA-Z.0-9]+)$').Groups[1].Value
         # Read the content of the file 
-        $fileContent = Get-Content $filePath
+        $fileContent = Get-Content -encoding byte $filePath
+        # [byte[]] $fileContent = [io.file]::ReadAllBytes($filePath)
 
         $encoded = base64Encode $fileContent
 
@@ -50,14 +53,20 @@
         }
 
         # Save the encoded file as a variable
-        $scriptContent = "`$bytes = `"$encoded""`n" 
-        $scriptContent += "`$DecodedText = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String(`$bytes))`n"
-        $scriptContent += "`$DecodedText | Out-File $fileName`n"
+        $scriptContent = "`$b64 = ""$encoded""`n" 
+        # Add row to decode the file
+        #$scriptContent += "`$DecodedText = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String(`$bytes))`n"
+        # $scriptContent += "`$DecodedText = [System.Convert]::FromBase64String(`$bytes)`n"
+        # Write the file to disk
+        #$scriptContent += "`$DecodedText | Out-File $fileName`n"
+        $scriptContent += "`$bytes = [System.Convert]::FromBase64String(`$b64)`n"
+        $scriptContent += "[io.file]::WriteAllBytes('$fileName', `$bytes)`n"
+        # Write the new script to a file 
         $scriptContent | Out-File $newFilePath
     }
 
 
-    $test = main "./example.txt"
+    $test = main "./example.jpg"
 
     #$test = base64Encode "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at risus nec enim tincidunt accumsan nec ut mi. Suspendisse ut viverra velit. Donec vitae maximus augue. Donec iaculis tincidunt interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ut ante eget laoreet. Suspendisse pellentesque tempor justo sed scelerisque. Suspendisse ut felis aliquet, condimentum elit eget, dictum nisl. Aliquam non nisl a tortor molestie condimentum. Sed a magna leo. "
     $test
