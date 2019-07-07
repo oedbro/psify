56a2befd4252578f04ef63147663d294dfdfd382
Oskar Edbro <oskar@edbro.se>
2019-07-07 19:55:19 +0200

    Fixed parameter input for script
    Note: to load the functions use . ./StringifyForWindows.ps1

--- StringifyForWindows.ps1
+++ StringifyForWindows.ps1
@@ -1,73 +1,70 @@
-    # param (
-    #    [string]$server = "http://defaultserver",
-    #    [Parameter(Mandatory=$true)][string]$username,
-    #    [string]$password = $( Read-Host "Input password, please" )
-    # )
+param (
+   [Parameter(Position=1)][string]$file = ""
+   )
 
-    function base64Encode {
-        # Ensure there are exactly one parameter, otherwise show usage.
-        if ($args.Count -ne 1) {
-            throw 'invalid variable passed to base64Encode'
-        }
-
-        # Convert to bytes
-        # $bytes = $args[0] # [System.Text.Encoding]::Ascii.GetBytes($args[0])
-
-        # base64Encode the content
-        # $EncodedText =[Convert]::ToBase64String($bytes)
-
-        $EncodedText = [Convert]::ToBase64String($args[0])
-
-        return $EncodedText
+function base64Encode {
+    # Ensure there are exactly one parameter, otherwise show usage.
+    if ($args.Count -ne 1) {
+        throw 'invalid variable passed to base64Encode'
     }
 
-    function base64Decode {
-        if ($args.Count -ne 1) {
-            throw 'invalid variable passed to base64Deccode'
-        }
+    # Convert to bytes
+    # $bytes = $args[0] # [System.Text.Encoding]::Ascii.GetBytes($args[0])
 
-        $DecodedText = [System.Convert]::FromBase64String($args[0])
-        return $DecodedText
+    # base64Encode the content
+    # $EncodedText =[Convert]::ToBase64String($bytes)
+
+    $EncodedText = [Convert]::ToBase64String($args[0])
+
+    return $EncodedText
+}
+
+function base64Decode {
+    if ($args.Count -ne 1) {
+        throw 'invalid variable passed to base64Deccode'
     }
 
-    function main {
-        # Ensure there are exactly one parameter, otherwise show usage.
-        if ($args.Count -ne 1) {
-            return 'usage "StringifyForWindows Filepath"'
-        }
-        
-        $filePath = $args[0]
-        $fileName = [regex]::match($filePath, '([a-zA-Z.0-9]+)$').Groups[1].Value
-        # Read the content of the file 
-        $fileContent = Get-Content -encoding byte $filePath
-        # [byte[]] $fileContent = [io.file]::ReadAllBytes($filePath)
+    $DecodedText = [System.Convert]::FromBase64String($args[0])
+    return $DecodedText
+}
 
-        $encoded = base64Encode $fileContent
+function psify {
+    # Ensure there are exactly one parameter, otherwise show usage.
+    if ($args.Count -ne 1) {
+        return 'usage "StringifyForWindows Filepath"'
+    }
+    
+    $filePath = $args[0]
+    $fileName = [regex]::match($filePath, '([a-zA-Z.0-9]+)$').Groups[1].Value
+    # Read the content of the file 
+    $fileContent = Get-Content -encoding byte $filePath
+    # [byte[]] $fileContent = [io.file]::ReadAllBytes($filePath)
 
-        $newFilePath = $filePath + ".ps1"
+    $encoded = base64Encode $fileContent
 
-        # If the file exists remove it
-        if (Test-Path $newFilePath) 
-        {
-            Remove-Item $newFilePath
-        }
+    $newFilePath = $filePath + ".ps1"
 
-        # Save the encoded file as a variable
-        $scriptContent = "`$b64 = ""$encoded""`n" 
-        # Add row to decode the file
-        #$scriptContent += "`$DecodedText = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String(`$bytes))`n"
-        # $scriptContent += "`$DecodedText = [System.Convert]::FromBase64String(`$bytes)`n"
-        # Write the file to disk
-        #$scriptContent += "`$DecodedText | Out-File $fileName`n"
-        $scriptContent += "`$bytes = [System.Convert]::FromBase64String(`$b64)`n"
-        $scriptContent += "[io.file]::WriteAllBytes('$fileName', `$bytes)`n"
-        # Write the new script to a file 
-        $scriptContent | Out-File $newFilePath
+    # If the file exists remove it
+    if (Test-Path $newFilePath) 
+    {
+        Remove-Item $newFilePath
     }
 
+    # Save the encoded file as a variable
+    $scriptContent = "`$b64 = ""$encoded""`n" 
+    # Add row to decode the file
+    #$scriptContent += "`$DecodedText = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String(`$bytes))`n"
+    # $scriptContent += "`$DecodedText = [System.Convert]::FromBase64String(`$bytes)`n"
+    # Write the file to disk
+    #$scriptContent += "`$DecodedText | Out-File $fileName`n"
+    $scriptContent += "`$bytes = [System.Convert]::FromBase64String(`$b64)`n"
+    $scriptContent += "[io.file]::WriteAllBytes('$fileName', `$bytes)`n"
+    # Write the new script to a file 
+    $scriptContent | Out-File $newFilePath
+}
 
-    $test = main "./example.jpg"
 
-    #$test = base64Encode "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at risus nec enim tincidunt accumsan nec ut mi. Suspendisse ut viverra velit. Donec vitae maximus augue. Donec iaculis tincidunt interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ut ante eget laoreet. Suspendisse pellentesque tempor justo sed scelerisque. Suspendisse ut felis aliquet, condimentum elit eget, dictum nisl. Aliquam non nisl a tortor molestie condimentum. Sed a magna leo. "
-    $test
-    #base64Decode $test
+if ($file -ne "") 
+{
+    psify $file
+}
